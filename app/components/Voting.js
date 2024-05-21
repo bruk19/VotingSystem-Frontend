ea185ff14521928b882e56598e51e4146d9531d0
Bruk Teshome <brukteshome21@gmail.com>
2024-05-21 13:37:14 +0300

    Add a deployed contract address to useEffect

--- app/components/Voting.js
+++ app/components/Voting.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {getWeb3} from "../web3";
+import { getWeb3 } from "../web3";
+import {abi, contractAddress} from "../constants/voting";
 
 function voting() {
 
   useEffect(() => {
     async function initialize() {
       const web3Instance = getWeb3();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      setWeb3(web3Instance);
+
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+      setContract(contractInstance);
     }
+    initialize();
   }, []);
   return (
     <div>voting</div>
